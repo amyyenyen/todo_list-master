c74db4f4b5b3b96922713cc8403891ec9ca26833
amyyenyen <amyyenyen@gmail.com>
2015-10-04 11:14:07 +0800

    Esther
    Category List and Category Item

---
+++ app/src/main/java/com/google/todolist/CategoryListAdapter.java
@@ -0,0 +1,30 @@
+package com.google.todolist;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Esther on 10/3/15.
+ */
+public class CategoryListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
